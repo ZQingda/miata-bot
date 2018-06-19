1bf6dfa15019f1efd8d93888b5452763210908e2
ZQingda <qingda.zou@gmail.com>
2018-06-19 19:20:52 -0400

    Config update

--- config-template.js
+++ config-template.js
@@ -1,3 +1,12 @@
 var exports = module.exports = {};
 
 exports.token = "Place token here";
+
+exports.getPrefix = function() {
+  return this.prefix || '#';
+}
+
+exports.setPrefix = function(prefix) {
+  this.prefix = prefix;
+  return this.prefix;
+}
